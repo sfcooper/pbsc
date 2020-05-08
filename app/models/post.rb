5d613db2d2da38f27c6d06f67a2e3f8bb9f162a5
Simon Cooper <cooper.sf@gmail.com>
2020-05-08 22:20:28 +0100

    adding travis

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,6 @@ class Post < ApplicationRecord
     has_rich_text :body
     has_many :post_tags
     has_many :tags, through: :post_tags
+    has_many_attached :photos
 
 end
--- db/migrate/20200504211531_create_post_tags.rb
+++ db/migrate/20200504211531_create_post_tags.rb
@@ -1,8 +1,8 @@
 class CreatePostTags < ActiveRecord::Migration[6.0]
   def change
     create_table :post_tags do |t|
-      t.int :post_id
-      t.int :tag_id
+      t.integer :post_id
+      t.integer :tag_id
 
       t.timestamps
     end
